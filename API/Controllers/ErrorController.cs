b0e30a778c467f267f28686acadca422f1697495
Ahmed Abd El-Rahman <45393469+AhmedMuhaameed@users.noreply.github.com>
2021-11-28 15:09:26 +0200

    Add Paging - Sorting - Filtering - Searching - CORS

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -11,7 +11,7 @@ namespace API.Controllers
     [Route("errors/{code}")]
     [ApiExplorerSettings(IgnoreApi =true)]
     public class ErrorController : BaseApiController
-    {
+    {   
         public IActionResult Error(int code)
         {
             return new ObjectResult(new ApiResponse(code));
--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Controllers;
 using API.Dtos;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entites;
 using Core.Interfaces;
@@ -13,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace SKINET.Controllers
 {
-    
+
     public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
@@ -32,17 +33,23 @@ namespace SKINET.Controllers
             _mapper = mapper;
         }
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+            var countspec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countspec);
 
             var products = await _productsRepo.ListAsync(spec);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,productParams.PageSize,totalItems,data));
         }
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
@@ -51,7 +58,7 @@ namespace SKINET.Controllers
 
             if (product == null) return NotFound(new ApiResponse(404));
 
-            return Ok(_mapper.Map<Product,ProductToReturnDto>(product));
+            return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
         }
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
